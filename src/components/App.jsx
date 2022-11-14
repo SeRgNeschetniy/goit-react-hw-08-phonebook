6c26647153e34defb25a7a85821f1b520451b6d8
SeRgNeschetniy <xdsergx@gmail.com>
2022-11-14 18:22:22 +0200

    add route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+
+const Login = lazy(() => import('pages/Login'));
+const Register = lazy(() => import('pages/Register'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+
+export default function Layout() {
+  return (
+    <Suspense fallback={null}>
+      <div>Layout</div>
+      <Outlet />
+    </Suspense>
+  );
+}
